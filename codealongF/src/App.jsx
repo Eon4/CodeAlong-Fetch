130fee41fd6593042e28118036e97e6ce233dbb7
Cecilia <109989926+Eon4@users.noreply.github.com>
2023-08-29 08:51:33 +0200

    Not finished

--- codealongF/src/App.jsx
+++ codealongF/src/App.jsx
@@ -1,33 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
+import { useEffect, useState } from 'react'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [data, setData] = useState([])
+
+  useEffect (() => {
+    const url = 'https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m'
+    fetch(url).then(res =>res.json()).then(data => setData(data))
+
+  },{})
+
+  console.log ("data: ", data)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+        <h3>Timezone:</h3>
+        <p> {data.timezome} </p>
+        <h3> Sunrise: </h3>
+        {data.daily.sunrise.map((item,index) =>{
+
+        } )}
+        </div>
+
     </>
   )
 }
